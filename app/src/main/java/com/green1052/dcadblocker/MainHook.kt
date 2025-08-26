7d71992c2a0613c7daa088cce74e28836da676ed
green1052 <green1052@pm.me>
2025-08-26 22:20:41 +0900

    1

--- app/src/main/java/com/green1052/dcadblocker/MainHook.kt
+++ app/src/main/java/com/green1052/dcadblocker/MainHook.kt
@@ -3,6 +3,7 @@ package com.green1052.dcadblocker
 import android.content.res.XResources
 import android.net.Uri
 import android.util.TypedValue
+import android.view.View
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import de.robv.android.xposed.IXposedHookInitPackageResources
@@ -49,8 +50,11 @@ class MainHook : IXposedHookLoadPackage, IXposedHookInitPackageResources {
 
         val adDimens = listOf(
             "ad_minimum_height",
+            "read_ad_minimum_height",
             "image_ad_height",
             "script_ad_size",
+            "image_ad_height",
+            "main_ad_live_best_spacing",
             "script_no_image_height",
             "script_no_image_width"
         )
